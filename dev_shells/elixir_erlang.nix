1e976501ff9e8914a55373d440b23c7909d97c3b
Alvaro Vilanova Vidal <alvivi@gmail.com>
2024-08-07 12:38:46 +0200

    Add exdoc to erlang builder

--- dev_shells/elixir_erlang.nix
+++ dev_shells/elixir_erlang.nix
@@ -4,9 +4,9 @@ inputs:
 let
   overlays = [
     (self: super: rec {
-      erlang = super.beam.interpreters.erlangR27.override {
-        version = inputs.erlangVersion;
+      erlang = super.beam.interpreters.erlang_27.override {
         sha256 = inputs.erlangSha256;
+        version = inputs.erlangVersion;
       };
 
       elixir = (super.beam.packagesWith erlang).elixir.override ({
--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
